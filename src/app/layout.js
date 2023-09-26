acab273565a09f3759f09c61a5b6771dcc5f6f3a
Shivamxagrawal <agrawalshivam57@gmail.com>
2023-09-26 16:58:24 +0530

    hoi hoi

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import Header from '@/components/header'
+import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import './globals.css'
 import { Inter } from 'next/font/google'
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import { useRef } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 
 
 export default function Home() {
