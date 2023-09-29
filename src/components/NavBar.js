dcef6a6e3f44570c9a027ce3eea196d637b28a97
tanish vashisth <tanishvashisth@gmail.com>
2023-09-29 23:02:44 +0530

    hoi hoi

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,36 +1,33 @@
-"use client"
-import React, { useState, useEffect } from 'react';
+"use client";
+import React, { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { motion } from "framer-motion";
 
 const navItems = [
-    {
-      path: "/",
-      name: "HOME",
-    },
-    {
-      path: "/business",
-      name: "BUSINESS",
-    },
-    {
-      path: "/sports",
-      name: "SPORTS",
-    },
-    {
-      path: "/national",
-      name: "NATIONAL",
+  {
+    path: "/",
+    name: "HOME",
+  },
+  {
+    path: "/business",
+    name: "BUSINESS",
+  },
+  {
+    path: "/sports",
+    name: "SPORTS",
+  },
+  {
+    path: "/national",
+    name: "NATIONAL",
+  },
+  {
+    path: "/technology",
+    name: "TECHNOLOGY",
+  },
+];
 
-    },
-    {
-      path: "/technology",
-      name: "TECHNOLOGY",
-    },
-  ];
-
-
-
-export default function NavBar () {
+export default function NavBar() {
   const [isScrolled, setIsScrolled] = useState(false);
 
   const handleScroll = () => {
@@ -42,9 +39,9 @@ export default function NavBar () {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
   let pathname = usePathname() || "/";
@@ -54,54 +51,169 @@ export default function NavBar () {
   }
 
   const [hoveredPath, setHoveredPath] = useState(pathname);
-
+    
   return (
-    <nav
+
+    <div className="grid grid-cols-2">
+        <nav
       className={`mx-auto container shadow-md  flex flexWrap p-5 flex-col md:flex-row items-center ${
-        isScrolled ? 'sticky top-0 z-30 w-full p-6 bg-black sm:px-4 shadow' : 'bg-blue-500'
+        isScrolled
+          ? "sticky top-0 z-30 w-full p-6 bg-black sm:px-4 shadow"
+          : "bg-blue-500"
       } py-4`}
     >
-       <div className="text-gray-600 body-font">
-      <nav className="md:ml-auto flex flexWrap items-center text-base justify-center">
-        {navItems.map((item, index) => {
-          const isActive = item.path === pathname;
-          
-          return (
-            <Link
-              key={item.path}
-              className={`px-4 py-2 rounded-md text-sm lg:text-base relative no-underline duration-300 ease-in ${
-                isActive ? "text-zinc-100" : "text-zinc-400"
-              }`}
-              data-active={isActive}
-              href={item.path}
-              onMouseOver={() => setHoveredPath(item.path)}
-              onMouseLeave={() => setHoveredPath(pathname)}
-            >
-              <span>{item.name}</span>
-              {item.path === hoveredPath && (
-                <motion.div
-                  className="absolute bottom-0 left-0 h-full  rounded-md -z-10"
-                  layoutId="navbar"
-                  aria-hidden="true"
-                  style={{
-                    width: "100%",
-                  }}
-                  transition={{
-                    type: "spring",
-                    bounce: 0.25,
-                    stiffness: 130,
-                    damping: 9,
-                    duration: 0.3,
-                  }}
-                />
-              )}
-            </Link>
-          );
-        })}
-      </nav>
-    </div>
-    </nav>
-  );
-};
+      <div className="text-gray-600 body-font">
+        <nav className="md:ml-auto flex flexWrap items-center text-base justify-center">
+          {navItems.map((item, index) => {
+            const isActive = item.path === pathname;
 
+            return (
+              <Link
+                key={item.path}
+                className={`px-4 py-2 rounded-md text-sm lg:text-base relative no-underline duration-300 ease-in ${
+                  isActive ? "text-zinc-100" : "text-zinc-400"
+                }`}
+                data-active={isActive}
+                href={item.path}
+                onMouseOver={() => setHoveredPath(item.path)}
+                onMouseLeave={() => setHoveredPath(pathname)}
+              >
+                <span>{item.name}</span>
+                {item.path === hoveredPath && (
+                  <motion.div
+                    className="absolute bottom-0 left-0 h-full  rounded-md -z-10"
+                    layoutId="navbar"
+                    aria-hidden="true"
+                    style={{
+                      width: "100%",
+                    }}
+                    transition={{
+                      type: "spring",
+                      bounce: 0.25,
+                      stiffness: 130,
+                      damping: 9,
+                      duration: 0.3,
+                    }}
+                  />
+                )}
+              </Link>
+            );
+          })}
+        </nav>
+
+
+      </div>
+      
+    </nav>
+
+        <div className={`mx-auto container shadow-md  flex flexWrap p-5 flex-col md:flex-row items-end ${
+        isScrolled
+          ? "sticky top-0 z-30 w-full p-6 bg-black sm:px-4 shadow"
+          : "bg-blue-500"
+      } py-4`}>
+        <form>
+          <div className="flex justify-items-end ml-72">
+            <button
+              id="dropdown-button"
+              data-dropdown-toggle="dropdown"
+              className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
+              type="button"
+            >
+              All categories{" "}
+              <svg
+                className="w-2.5 h-2.5 ml-2.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 1 4 4 4-4"
+                />
+              </svg>
+            </button>
+            <div
+              id="dropdown"
+              className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+            >
+              <ul
+                className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="dropdown-button"
+              >
+                <li>
+                  <button
+                    type="button"
+                    className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Sports
+                  </button>
+                </li>
+                <li>
+                  <button
+                    type="button"
+                    className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Business
+                  </button>
+                </li>
+                <li>
+                  <button
+                    type="button"
+                    className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    National
+                  </button>
+                </li>
+                <li>
+                  <button
+                    type="button"
+                    className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Technology
+                  </button>
+                </li>
+              </ul>
+            </div>
+            <div className="relative w-full">
+              <input
+                type="search"
+                id="search-dropdown"
+                className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+                placeholder="Search..."
+                required
+              />
+              <button
+                type="submit"
+                className="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                <svg
+                  className="w-4 h-4"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+                <span className="sr-only">Search</span>
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+
+    
+  );
+}
 
